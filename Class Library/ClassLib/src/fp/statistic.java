aefe27e97509339320367f8cb3dbadb9aa42503b
Rahmat Irfan <IrfanRahmat007@hotmail.com>
2014-12-27 12:00:19 +0700

    no message

--- Class Library/ClassLib/src/fp/statistic.java
+++ Class Library/ClassLib/src/fp/statistic.java
@@ -1,3 +1,5 @@
+package fp;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,5 +14,11 @@ public class statistic {
     int Table[][][];
     int TableIndex;
     int Turn;
+    int Count[];
+    int Status;
+    //1. Waiting for player
+    //2. Pick a hiding place
+    //3. Seek
+    //4. Game Over
     
 }
